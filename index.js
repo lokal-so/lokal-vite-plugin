8df89b7fa01a9139ebf33b2466b60c9791dc3b9d
Artfaith <the.f8er@gmail.com>
2024-08-09 18:23:38 +0200

    Small indentation fix

--- index.js
+++ index.js
@@ -2,29 +2,28 @@ import { Lokal } from 'lokal-js';
 
 // Vite plugin to log host and port
 export default function lokalTunnel(options = {}) {
-	const { tunnelName, lanAddress, publicAddress } = options;
+    const { tunnelName, lanAddress, publicAddress } = options;
 
-	return {
-		name: 'lokal-tunnel',
-		configureServer(server) {
-			server.httpServer.once('listening', async () => {
-				const address = server.httpServer.address();
-				const host = address.address.startsWith('::') ? 'localhost' : address.address;
-				const port = address.port;
+    return {
+        name: 'lokal-tunnel',
+        configureServer(server) {
+            server.httpServer.once('listening', async () => {
+                const address = server.httpServer.address();
+                const host = address.address.startsWith('::') ? 'localhost' : address.address;
+                const port = address.port;
+                const lokal = new Lokal();
 
-				const lokal = new Lokal();
-
-				await lokal
-					.newTunnel()
-					.setLocalAddress(`${host}:${port}`)
-					.setTunnelType('HTTP')
+                await lokal
+                    .newTunnel()
+                    .setLocalAddress(`${host}:${port}`)
+                    .setTunnelType('HTTP')
                     .setName(tunnelName)
-					.setPublicAddress(publicAddress)
-					.setLANAddress(lanAddress)
-					.showStartupBanner()
-					.ignoreDuplicate()
-					.create();
-			});
-		}
-	};
+                    .setPublicAddress(publicAddress)
+                    .setLANAddress(lanAddress)
+                    .showStartupBanner()
+                    .ignoreDuplicate()
+                    .create();
+            });
+        }
+    };
 }
